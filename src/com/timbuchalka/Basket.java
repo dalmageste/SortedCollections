c0dfdb72f6b2244d44ca8085b1560bb9397fee86
dalmageste <dalmageste@nyc.rr.com>
2021-09-12 18:44:10 -0400

    Added commentaire to file

--- src/com/timbuchalka/Basket.java
+++ src/com/timbuchalka/Basket.java
@@ -14,6 +14,7 @@ import java.util.TreeMap;
  * Contact author at:  http://timbuchalka.com
  * 
  */
+// commentaire added by cherif
 public class Basket {
     private final String name;
     private final Map<StockItem, Integer> list;
